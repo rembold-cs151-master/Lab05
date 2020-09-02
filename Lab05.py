26a4c939bc4f5c8ad74bc73a45d87c39134f9d9d
Jed Rembold <jedediah.r@gmail.com>
2020-09-02 12:33:32 -0700

    Add initial materials

---
+++ Lab05.py
@@ -0,0 +1,35 @@
+# File: Lab05.py
+
+"""
+Practice using the Portable Graphics Library
+"""
+
+from pgl import GWindow, GRect, GOval, GLine
+
+
+def create_picture():
+    """
+    Add your code to replicate the desired image in the space below.
+    I've started you off with a window.
+    """
+    GW_WIDTH = 500
+    GW_HEIGHT = 800
+
+    gw = GWindow(GW_WIDTH, GW_HEIGHT)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    create_picture()
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# Lab05
-Materials for Lab 5: An Intro to PGL
+# Lab 5: An Intro to PGL
+
+The main goal for today is to get some practice using the `pgl.py` library to draw some shapes to the screen. As such, your goal is to draw the scene shown in the lab instructions. I've included the `pgl.py` library here, as well as a rough starter file.
+
+No auto-checks on this one, since it is all visual. Just upload your finished code when you are done!
