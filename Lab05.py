1af8f80c6de79c51a8a06876d833a6e286469d91
Jed Rembold <jedediah.r@gmail.com>
2021-02-02 18:16:34 -0800

    Add new pgl version and adjust window size

--- Lab05.py
+++ Lab05.py
@@ -12,8 +12,8 @@ def create_picture():
     Add your code to replicate the desired image in the space below.
     I've started you off with a window.
     """
-    GW_WIDTH = 500
-    GW_HEIGHT = 800
+    GW_WIDTH = 400
+    GW_HEIGHT = 600
 
     gw = GWindow(GW_WIDTH, GW_HEIGHT)
 
